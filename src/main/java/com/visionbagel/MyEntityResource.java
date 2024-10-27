6419740b87b63ee4fadfd4ed64dd3cd7532994cc
quarkusio <no-reply@quarkus.io>
2024-10-27 10:18:32 +0000

    Initial commit

---
+++ src/main/java/com/visionbagel/MyEntityResource.java
@@ -0,0 +1,6 @@
+package com.visionbagel;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
