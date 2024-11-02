934d38e722a89ca8de17d45f6476dd7a8def236d
foxsir <838394225@qq.com>
2024-11-02 14:43:15 +0800

    chore

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,5 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+INSERT INTO public."user" (sex, when_created, when_deleted, when_modified, id, phone_number, username, avatar, password, role) VALUES (null, '2024-05-04 10:38:01.699416 +00:00', null, '2024-05-04 10:38:01.699467 +00:00', '36e149e3-ed38-42ef-83ca-fad1f9f10303', null, 'opensponsor', null, '$2a$10$q7ItLTa4RRHMjlYeUw98Bejt3CzQNW17lGmpx/.k3FoFXWZKpcKpG', null);
