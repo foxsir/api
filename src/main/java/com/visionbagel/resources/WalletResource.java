bd275a00401900d3c3dae7d65dc156a3992ece37
foxsir <838394225@qq.com>
2024-11-05 15:57:33 +0800

    update wallet api

---
+++ src/main/java/com/visionbagel/resources/WalletResource.java
@@ -0,0 +1,47 @@
+package com.visionbagel.resources;
+
+import com.visionbagel.entitys.User;
+import com.visionbagel.entitys.Wallet;
+import com.visionbagel.repositorys.UserRepository;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
+import java.util.Optional;
+
+@Path("/wallet")
+@RolesAllowed({ "User" })
+public class WalletResource {
+    @Inject
+    public UserRepository userRepository;
+
+    @GET()
+    @APIResponse(
+            responseCode = "200",
+            description = "get wallet info",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(
+                            implementation = Wallet.class
+                    )
+            )
+    )
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get() {
+        User user = userRepository.authUser();
+        Optional<Wallet> wallet = Wallet.find("user", user).firstResultOptional();
+
+        if(wallet.isPresent()) {
+            return Response.status(Response.Status.OK.getStatusCode()).entity(wallet.get()).build();
+        } else {
+            return Response.status(Response.Status.FORBIDDEN).entity(new Wallet()).build();
+        }
+    }
+}
