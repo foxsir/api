ec29166a75aacc31606fa9968ee0681a4943a4f4
foxsir <838394225@qq.com>
2024-11-20 17:54:37 +0800

    extraction upload method

--- src/main/java/com/visionbagel/resources/StorageResource.java
+++ src/main/java/com/visionbagel/resources/StorageResource.java
@@ -7,6 +7,7 @@ import com.visionbagel.payload.ResultOfData;
 import com.visionbagel.payload.SingleFileBody;
 import com.visionbagel.utils.FileTools;
 import com.visionbagel.utils.MediaTools;
+import com.visionbagel.utils.StorageForOSS;
 import jakarta.activation.MimetypesFileTypeMap;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.transaction.Transactional;
@@ -45,68 +46,71 @@ public class StorageResource {
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response update(@Valid @MultipartForm SingleFileBody data) throws IOException {
+    public Response upload(@Valid @MultipartForm SingleFileBody data) throws IOException {
 
-        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
-        String endpoint = "https://oss-cn-beijing.aliyuncs.com";
-        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-        // 填写Bucket名称，例如examplebucket。
-        String bucketName = "visionbagel";
-        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
-        // 填写Bucket所在地域。以华东1（杭州）为例，Region填写为cn-hangzhou。
-        String region = "cn-beijing";
+//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+//        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+//        // 填写Bucket名称，例如examplebucket。
+//        String bucketName = "visionbagel";
+//        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+//        // 填写Bucket所在地域。以华东1（杭州）为例，Region填写为cn-hangzhou。
+//        String region = "cn-beijing";
+//
+//        String accessKeyId = FileTools.getResource("alioss/accessKeyId.txt");
+//        String accessKeySecret = FileTools.getResource("alioss/accessKeySecret.txt");
+//
+//        assert accessKeyId != null;
+//        assert accessKeySecret != null;
+//        CredentialsProvider credentialsProvider = new DefaultCredentialProvider(accessKeyId, accessKeySecret);
+//
+//        // 创建OSSClient实例。
+//        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+//        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
+//        OSS ossClient = OSSClientBuilder.create()
+//                .endpoint(endpoint)
+//                .credentialsProvider(credentialsProvider)
+//                .clientConfiguration(clientBuilderConfiguration)
+//                .region(region)
+//                .build();
+//
+//        String suffix = MediaTools.getImageExtensionName(data.file).toLowerCase();
+//        String objectName = String.join(".", UUID.randomUUID().toString(), suffix);
+//
+//        try {
+//            // 创建PutObjectRequest对象。
+////            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+//            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, data.file);
+//
+//            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+//            // ObjectMetadata metadata = new ObjectMetadata();
+//            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+//            // metadata.setObjectAcl(CannedAccessControlList.Private);
+//            // putObjectRequest.setMetadata(metadata);
+//
+//            // 上传字符串。
+//            ObjectMetadata meta = new ObjectMetadata();
+//            meta.setContentType(String.format("image/%s", suffix));
+//            putObjectRequest.setMetadata(meta);
+//            ossClient.putObject(putObjectRequest);
+//        } catch (OSSException oe) {
+//            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+//                    + "but was rejected with an error response for some reason.");
+//            System.out.println("Error Message:" + oe.getErrorMessage());
+//            System.out.println("Error Code:" + oe.getErrorCode());
+//            System.out.println("Request ID:" + oe.getRequestId());
+//            System.out.println("Host ID:" + oe.getHostId());
+//        } catch (ClientException ce) {
+//            System.out.println("Caught an ClientException, which means the client encountered "
+//                    + "a serious internal problem while trying to communicate with OSS, "
+//                    + "such as not being able to access the network.");
+//            System.out.println("Error Message:" + ce.getMessage());
+//        } finally {
+//            ossClient.shutdown();
+//        }
 
-        String accessKeyId = FileTools.getResource("alioss/accessKeyId.txt");
-        String accessKeySecret = FileTools.getResource("alioss/accessKeySecret.txt");
-
-        assert accessKeyId != null;
-        assert accessKeySecret != null;
-        CredentialsProvider credentialsProvider = new DefaultCredentialProvider(accessKeyId, accessKeySecret);
-
-        // 创建OSSClient实例。
-        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
-        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
-        OSS ossClient = OSSClientBuilder.create()
-                .endpoint(endpoint)
-                .credentialsProvider(credentialsProvider)
-                .clientConfiguration(clientBuilderConfiguration)
-                .region(region)
-                .build();
-
-        String suffix = MediaTools.getImageExtensionName(data.file).toLowerCase();
-        String objectName = String.join(".", UUID.randomUUID().toString(), suffix);
-
-        try {
-            // 创建PutObjectRequest对象。
-//            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
-            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, data.file);
-
-            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
-            // ObjectMetadata metadata = new ObjectMetadata();
-            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
-            // metadata.setObjectAcl(CannedAccessControlList.Private);
-            // putObjectRequest.setMetadata(metadata);
-
-            // 上传字符串。
-            ObjectMetadata meta = new ObjectMetadata();
-            meta.setContentType(String.format("image/%s", suffix));
-            putObjectRequest.setMetadata(meta);
-            ossClient.putObject(putObjectRequest);
-        } catch (OSSException oe) {
-            System.out.println("Caught an OSSException, which means your request made it to OSS, "
-                    + "but was rejected with an error response for some reason.");
-            System.out.println("Error Message:" + oe.getErrorMessage());
-            System.out.println("Error Code:" + oe.getErrorCode());
-            System.out.println("Request ID:" + oe.getRequestId());
-            System.out.println("Host ID:" + oe.getHostId());
-        } catch (ClientException ce) {
-            System.out.println("Caught an ClientException, which means the client encountered "
-                    + "a serious internal problem while trying to communicate with OSS, "
-                    + "such as not being able to access the network.");
-            System.out.println("Error Message:" + ce.getMessage());
-        } finally {
-            ossClient.shutdown();
-        }
+        StorageForOSS storageForOSS = new StorageForOSS();
+        String objectName = storageForOSS.upload(data);
 
         String url = String.join("/", ossDomain, objectName);
         return Response
