de4f685f359b0e57b9121aa6786602da40b818a3
foxsir <838394225@qq.com>
2024-11-25 16:28:20 +0800

    add fal-ai/omnigen-v1 billing

--- src/main/java/com/visionbagel/utils/Biller.java
+++ src/main/java/com/visionbagel/utils/Biller.java
@@ -7,6 +7,7 @@ public class Biller {
         return switch (model) {
             case "fal-ai/flux/dev" -> BigDecimal.valueOf((double) (width * height) / (1000 * 1000) * (0.025 * 7 * 1.8));
             case "fal-ai/flux-pro/v1.1-ultra" -> BigDecimal.valueOf((0.06 * 7 * 1.8));
+            case "fal-ai/omnigen-v1" -> BigDecimal.valueOf((0.1 * 7 * 1.8));
             // case "fal-ai/flux-pro/new" -> BigDecimal.valueOf((double) (width * height) / (1000 * 1000) * (0.05 * 7 * 1.8));
             default -> BigDecimal.valueOf((double) (width * height) / (1000 * 1000) * (0.05 * 7 * 1.8));
         };
