2d73ce572deca8791ee46f93172d8b4f912b4ab2
foxsir <838394225@qq.com>
2024-12-19 17:34:58 +0800

    update

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew build
+
+echo "please paste and run command..."
+
+echo "docker push crpi-ghlb0lap2gtu95ve.cn-beijing.personal.cr.aliyuncs.com/visionbagel/visionbagel-api:latest" | pbcopy
